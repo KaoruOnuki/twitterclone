bbb521705a0cd5486622104a41b1aa523f4a728b
Kaoru Onuki <kaoru.onuki@gmail.com>
2018-05-14 01:34:27 +0000

    change on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root "howls#top"
   get "/howls", to: "howls#index" 
-  get "/howls/top", to: "howls#top" 
+  # get "/howls/top", to: "howls#top" 
   
   resources :howls do
     post :confirm, on: :collection
   end
+  
+  
   
 end
