15623b87799dbdf3ab5a26d8f72d908d2c088ad1
KaoruOnuki <kaoru.onuki@gmail.com>
2018-06-01 12:11:04 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,6 @@ Rails.application.configure do
   #   enable_starttls_auto: true
   # }
 
-  config.action_mailer.default_url_options = { host: 'shielded-springs-34497.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://shielded-springs-34497.herokuapp.com/' }
   config.action_mailer.delivery_method = :letter_opener
 end
